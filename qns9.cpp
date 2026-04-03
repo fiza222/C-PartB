d987b899e914326ddefd532366c1d2426c2ce0fc
fiza222 <fizaparween112@gmail.com>
2026-04-03 20:18:31 +0530

    Add files via upload

---
+++ qns9.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+                  using namespace std;
+
+                  int main(){
+
+                  char ch;
+                  int ASCII;
+
+                  cout<<"enter the character : ";
+                  cin>>ch;
+
+                  ASCII=(int)ch;
+
+                  cout<<"after converting : "<<ASCII;
+
+
+                  return 0;
+
+
+
+                  }
