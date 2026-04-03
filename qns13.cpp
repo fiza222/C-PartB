478d6c2fc3aa24490df244929a394c7b0a05608b
fiza222 <fizaparween112@gmail.com>
2026-04-03 20:20:41 +0530

    Add files via upload

---
+++ qns13.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+      using namespace std;
+
+      int main(){
+
+      float first_angle,second_angle,third_angle;
+
+      cout<<"enter the 1st angle : ";
+      cin>>first_angle;
+
+        cout<<"enter the 2nd angle : ";
+      cin>>second_angle;
+
+      third_angle = 180-(first_angle +second_angle);
+
+      cout<<" the 3rd angle is  : "<<third_angle;
+
+      return 0;
+
+
+
+
+      }
