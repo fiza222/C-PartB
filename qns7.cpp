d149000bc0712fdfd3227f238dd39cdc0aab2d00
fiza222 <fizaparween112@gmail.com>
2026-04-03 20:17:47 +0530

    Add files via upload

---
+++ qns7.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+         using namespace std;
+
+          int main(){
+
+          float a = 6.3;
+    int b;
+    b = (int)a;
+    cout<<"Before converting to int : "<<a;
+    cout<<"\nAfter converting to int : "<<b;
+    return 0;
+
+
+
+
+          }
