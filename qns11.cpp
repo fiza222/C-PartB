592ec5a36c41c4b85c8608f8591547520f4cf1f2
fiza222 <fizaparween112@gmail.com>
2026-04-03 20:19:38 +0530

    Add files via upload

---
+++ qns11.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+               using namespace std;
+               int main(){
+
+               float sub1,sub2,sub3,total_marks,percentage;
+
+
+               cout<<"enter the marks of sub1 : ";
+               cin>>sub1;
+
+               cout<<"enter the marks of sub2 : ";
+               cin>>sub2;
+
+               cout<<"enter the marks of sub3: ";
+               cin>>sub3;
+
+               total_marks=sub1+sub2+sub3;
+               percentage=(total_marks/300)*100;
+
+               cout<<"marks pf total subject is : "<<total_marks;
+               cout<<"\npercentage = "<<percentage;
+
+               return 0;
+
+
+
+               }
