16e3d63395a752c58fd01811f79272e0edc56f80
fiza222 <fizaparween112@gmail.com>
2026-04-03 20:17:18 +0530

    Add files via upload

---
+++ qns5.pp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 5;
+    float b = 2.5,result;
+    result = a + b;
+    cout<<"After adding the result is "<<result;
+
+
+
+
+
+
+}
